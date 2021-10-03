61abf1c03b8715f4e3e765373bbaef6c6e04b7c0
Tu Hu <UYHDK@leo-pharma.com>
2021-10-03 13:04:06 +0200

    Update de_space.sh

--- R/de_space.sh
+++ R/de_space.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mymanuscript
-rmodule
+cd /home/projects/ku_00015/people/tuhu/multiomics-ad-phd
+module load tools
+module load intel/perflibs/2020_update4 gcc/9.4.0 R/4.1.0
 Rscript R/de_space.R

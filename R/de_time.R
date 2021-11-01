1464404cedce4a49ae0dbc503c21031ab371c916
Tu Hu <tu.hu@outlook.com>
2021-11-01 16:57:05 +0100

    condition non-specific + specific for time dge

--- R/de_time.R
+++ R/de_time.R
@@ -21,81 +21,40 @@ se <- se[,!is.na(se$visit_quarter)]
 # se <- se %>% filter(subject %in% longitudinal_subject)
 
 DGE_time_d <-
-  map_dfr(seq_len(2), ~ tibble(
-          C1 = rep(c("LS", "LS", "NL"), 2),
-          C2 = rep(c("NL", "NN", "NN"), 2),
-          t = c(rep("visit", 3), rep("visit_quarter", 3))
-          )) %>%
-  mutate(condition_specific = c(rep(TRUE, 6), rep(FALSE, 6)),
-         contrast = paste0(C1, "vs", C2),
-         # se_exp = map2(C1, C2, ~ se[,se$skin_type %in% c(.x, .y)]),
+  tibble(C1 = rep(c("LS", "LS", "NL"), 2),
+         C2 = rep(c("NL", "NN", "NN"), 2),
+         t = c(rep("visit", 3), rep("visit_quarter", 3))) %>%
+  mutate(contrast = paste0(C1, "vs", C2),
          design_f = ifelse(C1 == "LS" & C2 == "NL",
                            paste("~", "subject", "+", t, "+", "skin_type"),
                            paste("~", "gender", "+", t, "+", "skin_type")),
-         design_f = ifelse(condition_specific == TRUE,
-                           paste(design_f, "+", "skin_type", "*", t),
-                           design_f),
+         design_f = paste(design_f, "+", "skin_type", "*", t),
          reduce_f = case_when(
-           (C1 == "LS") & (C2 == "NL") & (condition_specific == TRUE) ~ paste("~", "subject", "+", t, "+", "skin_type"),
-           (C2 == "NN") & (condition_specific == TRUE)  ~ paste("~", "gender", "+", t, "+", "skin_type"),
+           (C1 == "LS") & (C2 == "NL")  ~ paste("~", "subject", "+", t, "+", "skin_type"),
+           (C2 == "NN") ~ paste("~", "gender", "+", t, "+", "skin_type"),
            TRUE ~ "NULL"),
          se_exp = map2(C1, C2, ~ se[,se$skin_type %in% c(.x, .y)]),
-         deseq = map2(se_exp, design_f, ~ DESeqDataSet(.x, .y %>% as.formula)))
+         deseq = map2(se_exp, design_f, ~ DESeqDataSet(.x, .y %>% as.formula)),
+         deseq = map2(reduce_f, deseq, ~ DESeq(.y, test = "LRT", reduced = .x %>% as.formula, parallel = T)),
+         result_n = map2(deseq, t, ~ resultsNames(.x) %>% grep(paste0(.y, "_"), ., value = T)))
 
-DGE_time_d_t <-
+DGE_condition_specific <-
   DGE_time_d %>%
-  mutate(
-    deseq = pmap(list(condition_specific, reduce_f, deseq),
-                 function(x, y, z){
-                   if(x == TRUE){
-                     DESeq_res <- DESeq(z, test = "LRT", reduced = y %>% as.formula(), parallel = TRUE)
-                   }
-                   else{
-                     DESeq_res <- DESeq(z)
-                   }
-                   return(DESeq_res)
-                 }))
+  mutate(result = map(deseq, ~ results(.x) %>% as_tibble(rownames = "gene_name"))) %>%
+  select(t, contrast, result)
 
-
-
+DGE_condition_specific_non <-
+  DGE_time_d %>%
+  select(t, contrast, deseq, result_n) %>%
+  unnest(cols = "result_n") %>%
+  mutate(result = map2(deseq, result_n, ~ lfcShrink(.x, coef = .y, type = "apeglm"))) %>%
+  mutate(result = map(result, ~ .x %>% as_tibble(rownames = "gene_name"))) %>%
+  select(-deseq)
 
 
 # execuate jobs
 DGE_time <-
-  DGE_time_d %>%
-  mutate(
-    contrast = paste0(C1, "vs", C2),
-    se_exp = map2(C1, C2, ~ se[,se$skin_type %in% c(.x, .y)]),
-    design_f = ifelse(C1 == "LS" & C2 == "NL",
-                      paste("~", "subject", "+", t, "+", "skin_type", "+", "skin_type", "*", t),
-                      paste("~", "gender", "+", t, "+", "skin_type", "+", "skin_type", "*", t)),
-    reduce_f = ifelse(C1 == "LS" & C2 == "NL",
-                      paste("~", "subject", "+", t, "+", "skin_type"),
-                      paste("~", "gender", "+", t, "+", "skin_type")),
-    deseq = map2(se_exp, design_f, ~ DESeqDataSet(.x, .y %>% as.formula)))
-
-deseq <-
-  lapply(1:nrow(DGE_time), function(x){
-    deseq <- DESeq(DGE_time$deseq[[x]],
-                   test = "LRT",
-                   reduced = DGE_time$reduce_f[[x]] %>% as.formula,
-                   parallel = T)
-    return(deseq)
-         })
-
-res <-
-  lapply(1:length(deseq), function(x){
-    res <- results(deseq[[x]])
-    })
-
-res <-
-  lapply(1:length(res), function(x){
-    res <- res[[x]] %>% as.data.frame() %>% as_tibble(rownames = "gene_name")
-  })
-
-DGE_time <-
-  DGE_time %>% select(C1:contrast) %>%
-  mutate(res) %>% unnest() %>%
-  filter(padj < .05, abs(log2FoldChange) > 1)
-
+  list(DGE_condition_specific,
+       DGE_condition_specific_non)
+DGE_time$des <- "DGE_condition_specific: dge on time condition specific; DGE_condition_specific_non: dge on time condition non-specific"
 saveRDS(DGE_time, "data/dge_time.rds")
